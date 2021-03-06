f547dcd770bf76481f7e0248fa31949c342ce2ec
marcosradix <marcosradix@gmail.com>
2021-03-06 11:12:07 -0300

    ok

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,6 +1,7 @@
+import { login } from './../auth.actions';
+import { AppState } from './../../reducers/index';
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
-
 import {Store} from "@ngrx/store";
 
 import {AuthService} from "../auth.service";
@@ -20,7 +21,9 @@ export class LoginComponent implements OnInit {
   constructor(
       private fb:FormBuilder,
       private auth: AuthService,
-      private router:Router) {
+      private router:Router,
+      private store: Store<AppState>
+      ) {
 
       this.form = fb.group({
           email: ['test@angular-university.io', [Validators.required]],
@@ -34,7 +37,14 @@ export class LoginComponent implements OnInit {
   }
 
   login() {
-
+    const {email, password} = this.form.value;
+    this.auth.login(email, password).pipe(
+      tap((user) =>{
+        console.log(user);
+        this.store.dispatch(login({user}));
+        this.router.navigateByUrl('/courses');
+      })
+    ).subscribe(noop, ()=> alert('Login Failed'));
   }
 
 }
---
+++ src/app/auth/reducers/index.ts
@@ -0,0 +1,29 @@
+//what to do in a particular action
+import {
+  ActionReducer,
+  ActionReducerMap,
+  createFeatureSelector,
+  createReducer,
+  createSelector,
+  MetaReducer,
+  on
+} from '@ngrx/store';
+import { AuthActions } from '../actions.types';
+import { User } from '../model/user.model';
+
+export interface AuthState {
+user:User;
+}
+export const initialState: AuthState = {
+  user: undefined
+}
+
+export const  authReducer = createReducer(
+initialState,
+on(AuthActions.login, (state, action) => {
+  return {
+    user:action.user
+  }
+})
+);
+
